7099f663c80fcb44fbc30ed7053570769af91fe5
erinszilagyi <erinszilagyi@gmail.com>
2020-07-23 00:22:39 -0500

    Questions and answer widgets with basic result.

---
+++ lib/answer.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+  final String answerText;
+
+  Answer(this.selectHandler, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(minWidth: 300, maxWidth: 500),
+      // width: double.infinity,
+      // margin: EdgeInsets.all(50),
+      child: OutlineButton(
+          child: Text(answerText), 
+          onPressed: selectHandler,
+          borderSide: BorderSide(color: Colors.blue),
+          textColor: Colors.blue,
+          highlightedBorderColor: Colors.blue
+        ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+// import 'dart:io' show Platform;
+// import 'package:flutter/foundation.dart' show kIsWeb;
 
+// import './quiz.dart';
 import './question.dart';
-
-// void main() {
-//   runApp(QuizApp());
-// }
+import './answer.dart';
 
 void main() => runApp(QuizApp());
 
@@ -17,45 +17,52 @@ class QuizApp extends StatefulWidget {
 }
 
 class _QuizAppState extends State<QuizApp> {
+  final questions = const [
+      {
+        'questionText' : 'What is your favorite color?', 
+        'answers' : ['Black', 'Red', 'Green', 'White']
+      },
+      {
+        'questionText' : 'What is your favorite animal?', 
+        'answers' : ['Leopard', 'Koala Bear', 'Fennec Fox', 'Sloth']
+      },
+      {
+        'questionText' : 'What is your favorite food?', 
+        'answers' : ['Burgers', 'Tacos', 'Noodles', 'Pizza']
+      },
+    ];
+
   int _questionIndex = 0;
 
   void _answerQuestion(){
     setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-    print(_questionIndex);
+        _questionIndex = _questionIndex + 1;
+        print(_questionIndex);
+      });
+
+    // if (_questionIndex < questions.length - 1){
+      
+    // }
+
+    // print(Platform.isIOS);
+    // print(kIsWeb);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What is your favorite color?',
-      'What is your favorite animal?',
-    ];
-
-    // TODO: implement build
-    // throw UnimplementedError();
     return MaterialApp(home: Scaffold(
       appBar: AppBar(title: Text('Quiz App'),),
-      body: Column(children: [
+      body: _questionIndex < questions.length ? Column(children: [
         Question(
-          questions.elementAt(_questionIndex),
+          questions[_questionIndex]['questionText'],
         ),
-        RaisedButton(
-          child: Text('Answer 1'), 
-          onPressed: _answerQuestion,
-        ),
-        RaisedButton(
-          child: Text('Answer 2'), 
-          onPressed: _answerQuestion,
-        ),
-        RaisedButton(
-          child: Text('Answer 3'), 
-          onPressed: _answerQuestion,
-        ),
-      ],),
-
-    ),);
+        ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+          return Answer(_answerQuestion, answer);
+        }).toList(),
+      ],
+      ) : Center(child: Text('You did it!'),),
+    ),
+    );
   }
   
 }
