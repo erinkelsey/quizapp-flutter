83dc0def831e471c2ae2292e2b49020de34a3c03
erinkelsey <erinszilagyi@gmail.com>
2020-07-23 00:35:44 -0500

    add quiz and result widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 // import 'dart:io' show Platform;
 // import 'package:flutter/foundation.dart' show kIsWeb;
 
-// import './quiz.dart';
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(QuizApp());
 
@@ -17,7 +16,7 @@ class QuizApp extends StatefulWidget {
 }
 
 class _QuizAppState extends State<QuizApp> {
-  final questions = const [
+  final _questions = const [
       {
         'questionText' : 'What is your favorite color?', 
         'answers' : ['Black', 'Red', 'Green', 'White']
@@ -40,10 +39,6 @@ class _QuizAppState extends State<QuizApp> {
         print(_questionIndex);
       });
 
-    // if (_questionIndex < questions.length - 1){
-      
-    // }
-
     // print(Platform.isIOS);
     // print(kIsWeb);
   }
@@ -52,15 +47,13 @@ class _QuizAppState extends State<QuizApp> {
   Widget build(BuildContext context) {
     return MaterialApp(home: Scaffold(
       appBar: AppBar(title: Text('Quiz App'),),
-      body: _questionIndex < questions.length ? Column(children: [
-        Question(
-          questions[_questionIndex]['questionText'],
-        ),
-        ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
-          return Answer(_answerQuestion, answer);
-        }).toList(),
-      ],
-      ) : Center(child: Text('You did it!'),),
+      body: _questionIndex < _questions.length 
+          ? Quiz(
+              answerQuestion: _answerQuestion, 
+              questionIndex: _questionIndex, 
+              questions: _questions,
+            )
+          : Result(),
     ),
     );
   }
